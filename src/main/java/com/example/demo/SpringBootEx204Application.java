b590f86597e0b0db1e9edfe010cb9e8b53634cc6
GBTC408004UR <GBTC408004UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-14 10:50:21 -0400

    ex 204

---
+++ src/main/java/com/example/demo/SpringBootEx204Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEx204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEx204Application.class, args);
+	}
+
+}
